e3363165a6c9ad3a472122c36a783a980a5980a4
PhoenyxStar <ArthurPhoenix@gmail.com>
2016-10-05 16:18:57 -0400

    got seat selection GUI in semi-working state

--- MajorTom/src/main/webapp/resources/js/app.js
+++ MajorTom/src/main/webapp/resources/js/app.js
@@ -3,9 +3,53 @@
  */
 var app = angular.module("airline", ["ngRoute"]);
 
+app.controller("planeController", function() {
+	this.pilot = {
+		name: "Major Tom"
+	};
+	this.copilot = {
+		name: "Major Todd"
+	};
+	
+	
+	this.firstclassleft = [
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"}]
+	];
+	this.firstclassright = [[{
+		name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"
+	}]];
+	
+	
+	this.buisclassleft = [[{
+		name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"
+	}]];
+	this.buisclassright = [[{
+		name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"
+	}]];
+
+	
+	this.econclassleft = [[{
+		name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"
+	}]];
+	this.econclassright = [[{
+		name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"
+	}]];
+	
+});
 
 app.config(function($routeProvider) {
-	$routeProvider.when("/", {
+	$routeProvider
+	.when("/", {
 		templateUrl : "pages/landing.html"
 	})
 	.when("/flight", {
