0215846f6ddcf12c0fa648d129e441c291660c23
Kyle Garner <kgarner1@unca.edu>
2016-10-06 16:43:27 -0400

    Added e-mail functionality, still needs to be implemented on the view

--- MajorTom/src/main/java/com/gc/controller/TestController.java
+++ MajorTom/src/main/java/com/gc/controller/TestController.java
@@ -3,42 +3,58 @@ package com.gc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gc.model.MailManager;
 import com.gc.model.Seat;
 import com.gc.service.DataService;
 
 @RestController
 public class TestController {
-	
+
 	@Autowired
 	DataService dataService;
-	
-	@RequestMapping(value="testSave")
+
+	@RequestMapping(value = "testSave")
 	public String TestSave() {
 		System.out.println(dataService.findAirlineByName("Air Estonia"));
-		//dataService.saveFlight(new Flight());
+		// dataService.saveFlight(new Flight());
 		return "/TestPage.html";
 	}
-	
-	@RequestMapping(value="testTicket")
+
+	@RequestMapping(value = "testTicket")
 	public void testTicket() {
 		System.out.println("test the ticket");
 		Seat s = dataService.findSeatById(1);
 		System.out.println("seat:" + s);
 		System.out.println("ticket:" + dataService.findTicketBySeat(s));
 	}
-	
-	@RequestMapping(value="testSeats")
+
+	@RequestMapping(value = "testSeats")
 	public void testSeats() {
 		List<Seat> seats = dataService.findSeatsByFlight(dataService.findFlightById(1));
-		if(seats != null) {
+		if (seats != null) {
 			for (Seat s : seats) {
 				System.out.println("Seat: " + s);
 			}
 		}
+	}
+
+	@RequestMapping(value = "sendFakeMail")
+	public void sendMail() {
+		// Create the application context
+		ApplicationContext context = new FileSystemXmlApplicationContext("C:/Users/kyleg_000/JavaBatchSteve/team-ground-control/MajorTom/src/main/webapp/WEB-INF/spring-beans.xml");
+
+		// Get the mailer instance
+		MailManager mailer = (MailManager) context.getBean("mailService");
+
+		// Send a composed mail
+		mailer.sendMail("kyle.garner15@yahoo.com", "Test Subject", "Testing body");
+
+		// Send a pre-configured mail
+		//mailer.sendPreConfiguredMail("Exception occurred everywhere.. where are you ????");
 	}
 }
