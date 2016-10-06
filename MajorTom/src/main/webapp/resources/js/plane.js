d1d40c695a4512e8ccca1d1185814f7fe6e5822e
alle7522 <alle7522@vandals.uidaho.edu>
2016-10-06 09:57:00 -0400

    moved plane controller to seaparete file

---
+++ MajorTom/src/main/webapp/resources/js/plane.js
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+app.controller("planeController", function() {
+	this.pilot = {
+		name: "Major Tom"
+	};
+	this.copilot = {
+		name: "Major Todd"
+	};
+	
+	this.firstclass = [[{
+		name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"
+	}]];
+	
+	this.buisclass = [[{
+		name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"
+	}]];
+
+	this.econclass = [[{
+		name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"}],
+		[{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"},{name: "none"
+	}]];
+
+	
+});
