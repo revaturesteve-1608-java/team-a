8d586224aedb341feda165355946ec74bb351942
alle7522 <alle7522@vandals.uidaho.edu>
2016-10-07 11:29:00 -0400

    synchronizing files

--- MajorTom/src/main/java/com/gc/controller/PlaneController.java
+++ MajorTom/src/main/java/com/gc/controller/PlaneController.java
@@ -3,13 +3,11 @@ package com.gc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gc.model.Seat;
-import com.gc.model.Ticket;
 import com.gc.service.DataService;
 
 @RestController
