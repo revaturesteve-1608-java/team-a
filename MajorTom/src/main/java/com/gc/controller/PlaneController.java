283cb917de67d5cbf2dd5997abbb6e0811ed3f52
alle7522 <alle7522@vandals.uidaho.edu>
2016-10-10 10:00:47 -0400

    test push

--- MajorTom/src/main/java/com/gc/controller/PlaneController.java
+++ MajorTom/src/main/java/com/gc/controller/PlaneController.java
@@ -1,6 +1,7 @@
 package com.gc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,15 @@ public class PlaneController {
 	@Autowired
 	DataService dataService;
 	
+	/* pulls the list of seats by the given flight ID and returns it as a 
+	 * rest response with a 200 HTTP status, unless it's null, 
+	 * then returns a null and a not found status code
+	 */
 	@RequestMapping(value="")
 	public ResponseEntity<FormattedSeatsDTO> getFormattedFlightSeats(@PathVariable(value = "flightId") int flightId ) {
 		
+		FormattedSeatsDTO seats = new FormattedSeatsDTO(dataService.findSeatsByFlight(dataService.findFlightById(flightId)));
 		
-		
-		return null;//new ResponseEntity<FormattedSeatsDTO>(tick,tick==null?HttpStatus.NOT_FOUND:HttpStatus.ACCEPTED);
+		return new ResponseEntity<FormattedSeatsDTO>(seats, seats == null ? HttpStatus.NOT_FOUND : HttpStatus.ACCEPTED);
 	}
 }
