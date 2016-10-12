047a03470818521f531f56042f7b12270f322a56
Dexter Hu <Dexter Hu>
2016-10-12 11:50:47 -0400

    better speech box

--- MajorTom/src/main/webapp/resources/js/info.js
+++ MajorTom/src/main/webapp/resources/js/info.js
@@ -1,3 +1,6 @@
+/*
+ * For the speech bubble that appears when you click on a seat
+ */
 
 var infoApp = angular.module("airline");
 
@@ -6,8 +9,10 @@ infoApp.controller("infoController", function($scope, $rootScope, dataService) {
 	
 	// This event is triggered when a seat is clicked
     $rootScope.$on('seatClick', function(event, data) {
-        console.log("Hello from info controller" + JSON.stringify(data));
+        //console.log("Hello from info controller" + JSON.stringify(data));
+        var str = data.seatType.seatTypeName + " (Seat #" + data.seatId + ")\r\n\r\nFlight " + data.flight.flightId + "\r\nTo " + data.flight.destination.destinationName + " (" + data.flight.destination.destinationCode + 
+        	")\r\n\r\n" + data.flight.airline.name + "\r\n" + data.flight.airplane.airplaneName;
         $scope.infoVisible = true;
-        $scope.infoContents = JSON.stringify(data);
+        $scope.infoContents = str;
     })
 });
