fb415314f2d14dbea10f79830ca2f6b0f61a3ab0
Johnathan Cox <RyanC_Employment@hotmail.com>
2016-10-27 13:32:40 -0400

    Removed contructor order code smell.

--- MajorTomBoot/src/main/java/com/gc/MajorTomBootApplication.java
+++ MajorTomBoot/src/main/java/com/gc/MajorTomBootApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MajorTomBootApplication {
 
+	private MajorTomBootApplication(){} //Only needed to satisfy SonarQube.
+
 	public static void main(String[] args) {
 		SpringApplication.run(MajorTomBootApplication.class, args);
 	}
-
-	private MajorTomBootApplication(){} //Only needed to satisfy SonarQube.
 }
